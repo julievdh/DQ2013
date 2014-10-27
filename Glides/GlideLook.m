de80467f9137a8833760f041aa5d781053640bc9
julievdh <julievdh@mit.edu>
2014-10-26 20:51:16 -0700

    Pairwise Comparison
    Pairwise comparison for matched ZO points to determine whether or not
    measurement point matters when calculating Cd

--- Glides/GlideLook.m
+++ Glides/GlideLook.m
@@ -7,13 +7,16 @@
 %       Cd ZO2 Noren]
 load('GlideData')
 
-% plot data
+%% plot data
 
 % Figure 1: boxplot of drag coefficients by condition
 % Method 1
 figure(1)
 subplot(221)
 boxplot(-data(individual == 1,1),condition(individual == 1))
+text(1,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 0,1))))) 
+text(2,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 1,1)))))
+text(3,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 3,1)))))
 ylabel('C_d'); title('ZO1')
 
 subplot(222)
@@ -64,7 +67,7 @@ text(4,0,num2str(sum(~isnan(data(individual == 2 & condition == 5,8)))))
 ylim([-0.02 0.07])
 xlabel('Condition')
 
-[ax4,h3]=suplabel('Method 2: 30s Moving Average'  ,'t');
+[ax4,h3]=suplabel('Method 2: 1 s Moving Average'  ,'t');
 
 %% Average both ZO points using methods 1 and 2
 data(:,9) = nanmean(data(:,1:2)');
@@ -97,7 +100,7 @@ text(1,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 0,10)))))
 text(2,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 1,10)))))
 text(3,-0.01,num2str(sum(~isnan(data(individual == 1 & condition == 3,10)))))
 ylim([-0.02 0.14])
-title('30s MAV')
+title('1 s MAV')
 
 subplot(224)
 boxplot(data(individual == 2,10),condition(individual == 2))
@@ -109,3 +112,28 @@ ylim([-0.02 0.07])
 xlabel('Condition')
 
 [ax4,h3]=suplabel('Average of ZO1 and ZO2'  ,'t');
+
+
+%% Pairwise comparison for ZOs? 
+% (Ho = no significant difference between Cd estimated from ZO1 and ZO2)
+
+% find where have pairs
+matched = ~isnan(data(:,7)) & ~isnan(data(:,8));
+
+% create subsets of data
+matchdata = data(matched,:);
+matchindividual = individual(matched);
+matchcondition = condition(matched);
+
+% paired t test, bonferroni correction
+% for hoku, condition 0
+[h.hoku0,p.hoku0,ci,stats.hoku0] = ttest(matchdata(matchindividual == 1 & matchcondition == 0,7),matchdata(matchindividual == 1 & matchcondition == 0,8),'alpha',0.05/3);
+[h.hoku1,p.hoku1,ci,stats.hoku1] = ttest(matchdata(matchindividual == 1 & matchcondition == 1,7),matchdata(matchindividual == 1 & matchcondition == 1,8),'alpha',0.05/3);
+[h.hoku3,p.hoku3,ci,stats.hoku3] = ttest(matchdata(matchindividual == 1 & matchcondition == 3,7),matchdata(matchindividual == 1 & matchcondition == 3,8),'alpha',0.05/3);
+
+[h.liko0,p.liko0,ci,stats.liko0] = ttest(matchdata(matchindividual == 2 & matchcondition == 0,7),matchdata(matchindividual == 2 & matchcondition == 0,8),'alpha',0.05/4);
+[h.liko1,p.liko1,ci,stats.liko1] = ttest(matchdata(matchindividual == 2 & matchcondition == 1,7),matchdata(matchindividual == 2 & matchcondition == 1,8),'alpha',0.05/4);
+[h.liko3,p.liko3,ci,stats.liko3] = ttest(matchdata(matchindividual == 2 & matchcondition == 3,7),matchdata(matchindividual == 2 & matchcondition == 3,8),'alpha',0.05/4);
+[h.liko5,p.liko5,ci,stats.liko5] = ttest(matchdata(matchindividual == 2 & matchcondition == 5,7),matchdata(matchindividual == 2 & matchcondition == 5,8),'alpha',0.05/4);
+
+
