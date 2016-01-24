720396c7af18cfb328cba39c5c664ce24eb61988
julievdh <julievdh@mit.edu>
2016-01-24 12:46:54 -0800

    Pad video
    For where > 2 time steps between digitization points

---
+++ NoBoatVideo/vidPad.m
@@ -0,0 +1,12 @@
+function file = vidPad(file);
+% find gaps
+file_shift = file(2:end,1);
+ii = find(file(1:end-1,1)-file_shift < -0.3); %>1 time step? 3 time steps? 3
+
+mat2 = [NaN NaN NaN];
+% insert row
+for i = 1:length(ii)
+    file_pad = insertrows(file,mat2,ii(i));
+    file = file_pad;
+end
+
