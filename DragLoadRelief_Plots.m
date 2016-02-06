12756550876a101f3052d3702558eb99892e75b6
julievdh <julievdh@mit.edu>
2016-02-06 11:45:44 -0500

    Added plot on same axis

--- DragLoadRelief_Plots.m
+++ DragLoadRelief_Plots.m
@@ -1,4 +1,5 @@
 % Load and plot drag build/relief data from video analysis
+warning off
 
 % Kolohe
 load('Kolohe_DradLoadRelief');
@@ -7,56 +8,69 @@ load('Kolohe_DradLoadRelief');
 padAll
 
 figure(1); clf
-subplot(223); hold on;
-title('Kolohe - Drag Loading')
-plot(trainers(:,1),trainers(:,2),'k*')
-plot(load0(:,2),load0(:,3),'k','LineWidth',1.5)
-plot(loadA(:,2),loadA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',1.5)
-plot(loadA1(:,2),loadA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',1.5)
-plot(loadA2(:,2),loadA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',1.5)
-plot(loadA3(:,2),loadA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',1.5)
-plot(loadA4(:,2),loadA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',1.5)
+subplot('position',[0.1 0.1 0.4 0.4]); hold on;
+% title('Kolohe - Drag Loading')
+text(-18,13,'C','FontWeight','Bold','FontSize',18)
+plot(trainers(:,1),trainers(:,2),'k*','Markersize',8)
+plot(load0(:,2),load0(:,3),'k','LineWidth',2)
+plot(loadA(:,2),loadA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',2)
+plot(loadA1(:,2),loadA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',2)
+plot(loadA2(:,2),loadA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',2)
+plot(loadA3(:,2),loadA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',2)
+plot(loadA4(:,2),loadA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',2)
+adjustfigurefont
 
-subplot(224); hold on;
-title('Kolohe - Drag Relief')
-plot(trainers(:,1),trainers(:,2),'k*')
-plot(remove0(:,2),remove0(:,3),'k','LineWidth',1.5)
-plot(removeA(:,2),removeA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',1.5)
-plot(removeA1(:,2),removeA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',1.5)
-plot(removeA2(:,2),removeA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',1.5)
-plot(removeA3(:,2),removeA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',1.5)
-plot(removeA4(:,2),removeA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',1.5)
-
+subplot('position',[0.55 0.1 0.4 0.4]); hold on;
+% title('Kolohe - Drag Relief')
+text(-18,13,'D','FontWeight','Bold','FontSize',18)
+plot(trainers(:,1),trainers(:,2),'k*','Markersize',8)
+plot(remove0(:,2),remove0(:,3),'k','LineWidth',2)
+plot(removeA(:,2),removeA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',2)
+plot(removeA1(:,2),removeA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',2)
+plot(removeA2(:,2),removeA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',2)
+plot(removeA3(:,2),removeA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',2)
+plot(removeA4(:,2),removeA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',2)
+adjustfigurefont
 
 % Lono
 clear all
 load('Lono_DradLoadRelief');
 padAll
 
-subplot(221); hold on;
-title('Lono - Drag Loading')
-plot(trainers(:,1),trainers(:,2),'k*')
-plot(load0(:,2),load0(:,3),'k','LineWidth',1.5)
-plot(loadA(:,2),loadA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',1.5)
-plot(loadA1(:,2),loadA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',1.5)
-plot(loadA2(:,2),loadA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',1.5)
-plot(loadA3(:,2),loadA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',1.5)
-plot(loadA4(:,2),loadA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',1.5)
+subplot('position',[0.1 0.58 0.4 0.4]); hold on;
+% title('Lono - Drag Loading')
+text(-18,12,'A','FontWeight','Bold','FontSize',18)
+plot(trainers(:,1),trainers(:,2),'k*','Markersize',8)
+plot(load0(:,2),load0(:,3),'k','LineWidth',2)
+plot(loadA(:,2),loadA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',2)
+plot(loadA1(:,2),loadA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',2)
+plot(loadA2(:,2),loadA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',2)
+plot(loadA3(:,2),loadA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',2)
+plot(loadA4(:,2),loadA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',2)
 adjustfigurefont
 % legend('Trainers','0','A','A1','A2','A3','A4','location','NW')
 % legend('boxoff')
 
 
-subplot(222); hold on;
-title('Lono - Drag Relief')
-plot(trainers(:,1),trainers(:,2),'k*')
-plot(remove0(:,2),remove0(:,3),'k','LineWidth',1.5)
-plot(removeA(:,2),removeA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',1.5)
-plot(removeA1(:,2),removeA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',1.5)
-plot(removeA2(:,2),removeA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',1.5)
-plot(removeA3(:,2),removeA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',1.5)
-plot(removeA4(:,2),removeA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',1.5)
+subplot('position',[0.55 0.58 0.4 0.4]); hold on;
+% title('Lono - Drag Relief')
+text(-18,12,'B','FontWeight','Bold','FontSize',18)
+plot(trainers(:,1),trainers(:,2),'k*','Markersize',8)
+plot(remove0(:,2),remove0(:,3),'k','LineWidth',2)
+plot(removeA(:,2),removeA(:,3),'Color',[55/255 126/255 184/255],'LineWidth',2)
+plot(removeA1(:,2),removeA1(:,3),'Color',[255/255 127/255 0/255],'LineWidth',2)
+plot(removeA2(:,2),removeA2(:,3),'Color',[77/255 175/255 74/255],'LineWidth',2)
+plot(removeA3(:,2),removeA3(:,3),'Color',[152/255 78/255 163/255],'LineWidth',2)
+plot(removeA4(:,2),removeA4(:,3),'Color',[228/255 26/255 28/255],'LineWidth',2)
+adjustfigurefont
 
+[ax1,h1]=suplabel('X video coordinate');
+[ax2,h2]=suplabel('Y video coordinate','y');
+set(h2,'position',[-0.01 0.5 0],'FontSize',16)
+set(h1,'position',[0.5 -0.02 0],'FontSize',16)
+
+cd /Users/julievanderhoop/Documents/MATLAB/DQ/DQ2013/AnalysisFigs
+print -dpng LoadReliefLaps
 
 %% Duration of loading/unloading laps
 % calculate durations of loadings
@@ -77,19 +91,33 @@ dur_removeA3 = removeA3(end,1)-removeA3(1,1);
 dur_removeA4 = removeA4(end,1)-removeA4(1,1);
 
 figure(2); clf; hold on
-plot(0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
-plot(1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
-plot(2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
-plot(3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
-plot(4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
-plot(5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+axesPosition = [110 40 200 200];  %# Axes position, in pixels
+set(gca,'Position',axesPosition);
+h1 = axes('Color','w','XColor','k','YColor','k',...
+          'YLim',[10 50],'Xlim',[-0.5 11.5],...
+          'XTick',[],'XTickLabel',[],...
+          'NextPlot','add');
+h2 = axes('Color','none','XColor','k','YColor','k',...
+          'YLim',[0 2],'Xlim',[-0.5 11.5],...
+          'Ytick',[0 .25 .5 1.25],...
+          'Yticklabels',{'0','25','50','125'},...
+          'Yaxislocation','right','NextPlot','add');
 
-plot(11,dur_remove0,'ko','MarkerFaceColor','k','MarkerSize',10)
-plot(10,dur_removeA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
-plot(9,dur_removeA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
-plot(8,dur_removeA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
-plot(7,dur_removeA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
-plot(6,dur_removeA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+plot(h1,0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h1,1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h1,2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h1,3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h1,4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h1,5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+
+plot(h1,11,dur_remove0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h1,10,dur_removeA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h1,9,dur_removeA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h1,8,dur_removeA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h1,7,dur_removeA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h1,6,dur_removeA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+
+ylabel(h1,'Lap Duration (s)')
 %% percent change between tag condition and CONTROL
 p0A = (dur_loadA-dur_load0)/dur_load0;
 p0A1= (dur_loadA1-dur_load0)/dur_load0;
@@ -105,11 +133,42 @@ pA10 = (dur_removeA1-dur_remove0)/dur_remove0;
 pA0 = (dur_removeA-dur_remove0)/dur_remove0;
 
 % plot
-plot(1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
-plot(6:10,[pA40 pA30 pA20 pA10 pA0],'k')
+plot(h2,1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
+plot(h2,6:10,[pA40 pA30 pA20 pA10 pA0],'k')
+
+%% figure(3): Plot increasing and decreasing on same axis
+figure(3); clf; hold on
+axesPosition = [110 40 200 200];  %# Axes position, in pixels
+set(gca,'Position',axesPosition);
+h3 = axes('Color','w','XColor','k','YColor','k',...
+          'YLim',[10 50],'Xlim',[-0.5 5.5],...
+          'XTick',[],'XTickLabel',[],...
+          'NextPlot','add');
+h4 = axes('Color','none','XColor','k','YColor','k',...
+          'YLim',[0 2],'Xlim',[-0.5 5.5],...
+          'Ytick',[0 .25 .5 1.25],...
+          'Yticklabels',{'0','25','50','125'},...
+          'Yaxislocation','right','NextPlot','add');
+
+plot(h3,0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h3,1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h3,2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h3,3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h3,4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h3,5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+
+plot(h3,0,dur_remove0,'o','MarkerEdgeColor','k','MarkerSize',10,'LineWidth',2)
+plot(h3,1,dur_removeA,'^','MarkerEdgeColor',[55/255 126/255 184/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,2,dur_removeA1,'o','MarkerEdgeColor',[255/255 127/255 0/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,3,dur_removeA2,'d','MarkerEdgeColor',[77/255 175/255 74/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,4,dur_removeA3,'o','MarkerEdgeColor',[152/255 78/255 163/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,5,dur_removeA4,'s','MarkerEdgeColor',[228/255 26/255 28/255],'MarkerSize',10,'LineWidth',2)
+
+plot(h4,1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
+plot(h4,5:-1:1,[pA40 pA30 pA20 pA10 pA0],'k')
 
 %% Kolohe
-clear all; load('Kolohe_DradLoadRelief');
+keep h1 h2 h3 h4; load('Kolohe_DradLoadRelief');
 
 % calculate durations of loadings
 dur_load0 = load0(end,1)-load0(1,1);
@@ -129,19 +188,19 @@ dur_removeA3 = removeA3(end,1)-removeA3(1,1);
 dur_removeA4 = removeA4(end,1)-removeA4(1,1);
 
 figure(2); 
-plot(0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
-plot(1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
-plot(2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
-plot(3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
-plot(4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
-plot(5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+plot(h1,0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h1,1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h1,2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h1,3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h1,4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h1,5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
 
-plot(11,dur_remove0,'ko','MarkerFaceColor','k','MarkerSize',10)
-plot(10,dur_removeA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
-plot(9,dur_removeA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
-plot(8,dur_removeA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
-plot(7,dur_removeA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
-plot(6,dur_removeA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+plot(h1,11,dur_remove0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h1,10,dur_removeA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h1,9,dur_removeA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h1,8,dur_removeA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h1,7,dur_removeA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h1,6,dur_removeA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
 %% percent change between tag condition and CONTROL
 p0A = (dur_loadA-dur_load0)/dur_load0;
 p0A1= (dur_loadA1-dur_load0)/dur_load0;
@@ -157,35 +216,36 @@ pA10 = (dur_removeA1-dur_remove0)/dur_remove0;
 pA0 = (dur_removeA-dur_remove0)/dur_remove0;
 
 % plot
-plot(1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
-plot(6:10,[pA40 pA30 pA20 pA10 pA0],'k')
+plot(h2,1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
+plot(h2,6:10,[pA40 pA30 pA20 pA10 pA0],'k')
 
-return 
-
-%% figure(2): Lono Only
-figure(2); set(gcf,'position',[130 241 1150 385]); clf
-subplot(121); hold on;
-title('Lono - Drag Loading')
-plot(trainers(:,1),trainers(:,2),'k^','MarkerFaceColor','k')
-plot(load0(:,2),load0(:,3),'.','MarkerSize',10)
-plot(loadA(:,2),loadA(:,3),'.','MarkerSize',10)
-plot(loadA1(:,2),loadA1(:,3),'.','MarkerSize',10)
-plot(loadA2(:,2),loadA2(:,3),'.','MarkerSize',10)
-plot(loadA3(:,2),loadA3(:,3),'.','MarkerSize',10)
-plot(loadA4(:,2),loadA4(:,3),'.','MarkerSize',10)
-legend('Trainers','C','T','T+2','T+4','T+6','T+8','location','NW')
-legend('boxoff')
-
-
-subplot(122); hold on;
-title('Lono - Drag Relief')
-plot(trainers(:,1),trainers(:,2),'k^','MarkerFaceColor','k')
-plot(remove0(:,2),remove0(:,3),'.','MarkerSize',10)
-plot(removeA(:,2),removeA(:,3),'.','MarkerSize',10)
-plot(removeA1(:,2),removeA1(:,3),'.','MarkerSize',10)
-plot(removeA2(:,2),removeA2(:,3),'.','MarkerSize',10)
-plot(removeA3(:,2),removeA3(:,3),'.','MarkerSize',10)
-plot(removeA4(:,2),removeA4(:,3),'.','MarkerSize',10)
+warning off
+set(gca,'xtick',0:11,'xticklabels',{'C','T','T+2','T+4','T+6','T+8','T+8','T+6','T+4','T+2','T','C'})
+ylabel('Percent increase over control')
 
 adjustfigurefont
-print('Lono_DragLoadRelief.tiff','-dtiff','-r300')
+
+print -dpng LoadReliefSpeed
+
+%% Figure 3
+figure(3);
+plot(h3,0,dur_load0,'ko','MarkerFaceColor','k','MarkerSize',10)
+plot(h3,1,dur_loadA,'k^','MarkerFaceColor',[55/255 126/255 184/255],'MarkerSize',10)
+plot(h3,2,dur_loadA1,'ko','MarkerFaceColor',[255/255 127/255 0/255],'MarkerSize',10)
+plot(h3,3,dur_loadA2,'kd','MarkerFaceColor',[77/255 175/255 74/255],'MarkerSize',10)
+plot(h3,4,dur_loadA3,'ko','MarkerFaceColor',[152/255 78/255 163/255],'MarkerSize',10)
+plot(h3,5,dur_loadA4,'ks','MarkerFaceColor',[228/255 26/255 28/255],'MarkerSize',10)
+
+plot(h3,0,dur_remove0,'o','MarkerEdgeColor','k','MarkerSize',10,'LineWidth',2)
+plot(h3,1,dur_removeA,'^','MarkerEdgeColor',[55/255 126/255 184/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,2,dur_removeA1,'o','MarkerEdgeColor',[255/255 127/255 0/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,3,dur_removeA2,'d','MarkerEdgeColor',[77/255 175/255 74/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,4,dur_removeA3,'o','MarkerEdgeColor',[152/255 78/255 163/255],'MarkerSize',10,'LineWidth',2)
+plot(h3,5,dur_removeA4,'s','MarkerEdgeColor',[228/255 26/255 28/255],'MarkerSize',10,'LineWidth',2)
+
+plot(h4,1:5,[p0A p0A1 p0A2 p0A3 p0A4],'k')
+plot(h4,5:-1:1,[pA40 pA30 pA20 pA10 pA0],'k')
+
+print -dpng LoadReliefSpeed_2
+
+
