01474ef0863039f5a13aea321b8f245300f5b4ca
julievdh <julievdh@mit.edu>
2016-03-17 10:09:17 -0400

    Added tag weight and surface area

--- Glides/GlideCheck_2a.m
+++ Glides/GlideCheck_2a.m
@@ -1,15 +1,16 @@
 close all; clear all; clc
 
-% Import glide .csv 
+% Import glide .csv
 cd /Users/julievanderhoop/Documents/NOPPTagDrag/DolphinQuest2013/Glides/CUT
 A = importdata('UWC_Liho_289_2a.csv',',',2);
+Cond = 1;
 ZO1 = A.data(:,1:5);
 ZO2 = A.data(:,6:10);
 
 ZO1 = ZO1(~isnan(ZO1(:,1)),:);
 ZO2 = ZO2(~isnan(ZO2(:,1)),:);
 
-%% 
+%%
 % plot velocity from different body points
 figure(1); hold on
 set(gcf,'Position',[290 120 1000 520])
@@ -34,13 +35,29 @@ if ZO2dur < 1
 end
 
 %% SET PARAMETERS
+
+% Add condition-specific tag weights and surface areas
+if Cond == 1
+    Mt = 0.250; % tag weighs 250 g (weighed on necropsy scale 17 Mar 2016)
+    SAt = 0.024; % tag surface area from CFD sims
+else if Cond == 3
+        Mt = 0.350+(0.01)*4; % GET ACTUAL WEIGHT OF ELEMENTS
+        SAt = 0.024+(0.0105)*4; % tag and element surface area from CFD sims
+    else if Cond == 5
+            Mt = 0.350+(0.01)*8;
+            SAt = 0.024+(0.0105)*4;
+        else Mt = 0; SAt = 0;
+        end
+    end
+end
 % Mb = body mass (kg)
-    % Liho = 339.7 lbs = 154 kg
-    % Hoku = 388 lbs = 176 kg
-Mb = 154*1.06;
-% Ma = entrained water mass (kg)
-% SAw = reference area (m^2) % estimated wetted surface area
-SAw = 0.08*Mb.^0.65;
+% Liho = 339.7 lbs = 154 kg
+% Hoku = 388 lbs = 176 kg
+% Ma = entrained water mass (kg) = 1.06
+Mb = (154+Mt)*1.06;
+% SAw = reference area (m^2) % estimated wetted surface area from Fish 1993
+% Add SAt = surface area of tag from CFD
+SAw = (0.08*Mb.^0.65)+SAt;
 % rho = water density (kg m^3) - for 24C water, salinity 31 from Oct 16, 17
 % as measured at DQO (see water quality for glide.jrl.o13014.xlsx)
 rho = 1021;
@@ -64,5 +81,5 @@ Cd_ZO1_mn = nanmean(Cd_ZO1);
 Cd_ZO2 = (2*abs(Mb.*ZO2_filt_ax))./(rho*SAw*ZO2_filt(2:end,4).^2);
 Cd_ZO2_mn = nanmean(Cd_ZO2);
 
-% to export
-CDmav = abs([Cd_ZO1_mn Cd_ZO2_mn mean([sum(isnan(ZO2_filt(:,4))) sum(isnan(ZO1_filt(:,4)))])]);
+% to export: CD from ZO1, from ZO2, duration of glide
+CDmav = abs([Cd_ZO1_mn Cd_ZO2_mn mean([sum(isnan(ZO2_filt(:,4))) sum(isnan(ZO1_filt(:,4)))])])
