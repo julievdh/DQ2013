98a5d0f31df03e281d3ace41d22195c5e891e193
julievdh <julievdh@mit.edu>
2016-04-26 10:24:56 -0400

    Plot V, A, Cd
    To show how things are calculated and averaged over time

--- Glides/GlideCheck_2a.m
+++ Glides/GlideCheck_2a.m
@@ -22,14 +22,15 @@ V2 = sqrt(Vx2.^2 + Vy2.^2);
 
 %%
 % plot velocity from different body points
-figure(1); hold on
+figure(1); clf; 
 set(gcf,'Position',[290 120 1000 520])
+subplot(311); hold on
 % plot(fluke(:,1),fluke(:,4))
-plot(ZO1(:,1),ZO1(:,4))
-plot(ZO2(:,1),ZO2(:,4))
+plot(ZO1(:,1),ZO1(:,4),'o'); plot(ZO2(:,1),ZO2(:,4),'o'); % plot velocity from tracker
+plot(ZO1(2:end,1),V1); plot(ZO2(2:end,1),V2); % plot velocity calculated
 xlim([min(ZO1(:,1)),max(ZO2(:,1))])
 
-xlabel('Time (s)'); ylabel('Velocity (m/s)'); adjustfigurefont; box on
+ylabel('Velocity (m/s)'); box on
 
 legend('Zinc Oxide 1','Zinc Oxide 2','Location','SW')
 legend boxoff
@@ -80,16 +81,41 @@ rho = 1021;
 windowSize = 30; % 30 frames = 1s
 ZO1_filt = filter(ones(1,windowSize)/windowSize,1,ZO1);
 ZO2_filt = filter(ones(1,windowSize)/windowSize,1,ZO2);
+plot(ZO1_filt(:,1),ZO1_filt(:,4)); % plot filtered velocity
+plot(ZO2_filt(:,1),ZO2_filt(:,4));
+% calcluate for calculated velocity (not tracker velocity)
+V1_filt = filter(ones(1,windowSize)/windowSize,1,[ZO1(2:end,1) V1]);
+V2_filt = filter(ones(1,windowSize)/windowSize,1,[ZO2(2:end,1) V2]);
+V1_filt(1:29) = NaN; V2_filt(1:29) = NaN;
+plot(V1_filt(:,1),V1_filt(:,2),V2_filt(:,1),V2_filt(:,2))
 
+subplot(312); hold on
 % compute acceleration from smoothed velocity
-ZO1_filt_ax = (ZO1_filt(2:end,4) - ZO1_filt(1:end-1,4))/(1/29.97);
+ZO1_filt_ax = (ZO1_filt(2:end,4) - ZO1_filt(1:end-1,4))/(1/29.97); % on tracker data
 ZO2_filt_ax = (ZO2_filt(2:end,4) - ZO2_filt(1:end-1,4))/(1/29.97);
+plot(ZO1_filt(2:end,1),ZO1_filt_ax,ZO2_filt(2:end,1),ZO2_filt_ax)
+A1_filt = diff(V1_filt(:,2))./diff(V1_filt(:,1)); % on calculated data
+A2_filt = diff(V2_filt(:,2))./diff(V2_filt(:,1));
+plot(V1_filt(2:end,1),A1_filt,V2_filt(2:end,1),A2_filt)
+% plot mean A
+plot([min(ZO1(:,1)),max(ZO2(:,1))],[nanmean([A1_filt; A2_filt]) nanmean([A1_filt; A2_filt])],'color',[0.75 0.75 0.75])
+ylabel('Acceleration (m/s^2)'); 
+adjustfigurefont; box on
+xlim([min(ZO1(:,1)),max(ZO2(:,1))])
 
+subplot(313); hold on
 % Calculate Cd from drag (D = Mb*accel)
 Cd_ZO1 = (2*abs(Mb.*ZO1_filt_ax))./(rho*SAw*ZO1_filt(2:end,4).^2);
 Cd_ZO1_mn = nanmean(Cd_ZO1);
 Cd_ZO2 = (2*abs(Mb.*ZO2_filt_ax))./(rho*SAw*ZO2_filt(2:end,4).^2);
 Cd_ZO2_mn = nanmean(Cd_ZO2);
+plot(V1_filt(:,1),Cd_ZO1,V2_filt(:,1),Cd_ZO2);
+% plot mean Cd
+h = plot([min(ZO1(:,1)),max(ZO2(:,1))],[mean([Cd_ZO1_mn,Cd_ZO2_mn]) mean([Cd_ZO1_mn,Cd_ZO2_mn])],'color',[0.75 0.75 0.75]);
+
+xlabel('Time (s)'); ylabel('Drag Coefficient, C_d'); 
+adjustfigurefont; box on
+xlim([min(ZO1(:,1)),max(ZO2(:,1))])
 
 % to export: CD from ZO1, from ZO2, duration of glide
 CDmav = abs([Cd_ZO1_mn Cd_ZO2_mn mean([ZO1dur ZO2dur]) mean([ZO1(2,4) ZO2(2,4)]) mean([ZO1(end,4) ZO2(end,4)])]);
