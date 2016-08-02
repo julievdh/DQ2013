79223a70f96fd89032c0643e51a0546d2c8ed85d
julievdh <julievdh@mit.edu>
2016-08-02 19:57:13 -0400

    Save format

--- Kolohe13_269_C_O2Fig.m
+++ Kolohe13_269_C_O2Fig.m
@@ -7,8 +7,10 @@ load('Kolohe13_269_C')
 %% plot selected times
 figure(1); clf; hold on
 set(gcf,'position',[1631         390        1115         154]) 
-plot(Interval(3720:79179),O2(3720:79179))
-plot(Interval(170379:222379),O2(170379:222379))
+set(gcf,'paperpositionmode','auto')
+% plot(Interval(3720:79179),O2(3720:79179))
+% plot(Interval(170379:222379),O2(170379:222379))
+plot(Interval(3720:222379),O2(3720:222379))
 
 cd /Users/julievanderhoop/Documents/MATLAB/DQ/DQ2013/AnalysisFigs
 print('Kolohe13_269_C_raw','-dsvg','-r300')
