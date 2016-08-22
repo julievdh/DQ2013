3536a65a1bf81093fe43c0d82655fdfcd9ebd202
julievdh <julievdh@mit.edu>
2016-08-22 08:03:38 -0400

    Add Frontal Area

--- Glides/AcceptableTagSize.m
+++ Glides/AcceptableTagSize.m
@@ -53,13 +53,17 @@ NaN
 tagWa3 = Wa*0.03;
 tagWa1 = Wa*0.01;
 
+tagFa2 = Fa*0.02;
+tagFa10 = Fa*0.1;
+
+
 %%
 %# Some initial computations:
 axesPosition = [110 40 200 200];  %# Axes position, in pixels
 yWidth = 30;                      %# y axes spacing, in pixels
 
 %# Create the figure and axes:
-figure(2); clf; hold on
+figure(1); clf; hold on
 set(gca,'Position',axesPosition)
 h1 = axes('Color','w','XColor','k','YColor',[123/255 50/255 148/255],...
           'YLim',[0 100],'Xlim',[0.005 2],'xscale','log','yscale','log',...
@@ -91,11 +95,38 @@ tag268 = 0.01785;
 tag333 = 0.00739;
 plot(h1,[tag268 tag268],[0.58 100],'k:')
 plot(h1,[tag333 tag333],[0.58 100],'k:')
-%%
 
 % save
 cd /Users/julievanderhoop/Documents/MATLAB/DQ/DQ2013/AnalysisFigs
 print('Glide_AcceptableTagSize','-dsvg','-r300')
+
+%% add second figure with FA
+figure(2); clf; hold on
+set(gca,'Position',axesPosition)
+h1 = axes('Color','w','XColor','k','YColor',[123/255 50/255 148/255],...
+          'YLim',[0 10],'Xlim',[0.005 2],'xscale','log','yscale','log',...
+          'NextPlot','add');
+h2 = axes('Color','none','XColor','k','YColor',[221/255 90/255 13/255],...
+          'YLim',[0 20],'Xlim',[0.005 2],'xscale','log','yscale','log',...
+          'XTick',[],'XTickLabel',[],...
+          'Ytick',[0 2 5 10 15 20],...
+          'Yaxislocation','right','NextPlot','add');
+
+xlabel(h1,'Tag Frontal Area (m^2)');
+ylabel(h2,'Body Length (m)');
+ylabel(h1,'Frontal Area (m^2)');
+
+plot(h1,tagFa10,Fa,'o','markerfacecolor',[123/255 50/255 148/255],'markeredgecolor','k')
+plot(h1,tagFa2,Fa,'o','markeredgecolor',[123/255 50/255 148/255])
+
+plot(h2,tagFa10,bodylength,'o','markerfacecolor',[221/255 90/255 13/255],'markeredgecolor','k')
+plot(h2,tagFa2,bodylength,'o','markeredgecolor',[221/255 90/255 13/255])
+
+
+%% add tags on market
+
+
+
 
 %% Figure S2 - wetted area increase vs Cd increase
 % increase in wetted area from DTAG3
@@ -109,21 +140,25 @@ GlideTVInc = [26.2 26.2 NaN NaN 75.6 75.6 185.8 185.8];
 GlideVFInc = [7.9 7.9 NaN NaN 126.4 126.4 192.5 192.5];
 
 figure(3); clf; hold on
+plot([mean(DtagWAInc(1:2))  mean(DtagWAInc(3:4)) mean(DtagWAInc(5:6))],[CFDInc(1) CFDInc(3) CFDInc(5)],'k-')
 plot(DtagWAInc(1:2),CFDInc(1:2),'k^','markerfacecolor',[55/255 126/255 184/255],'markersize',8); % tag
 plot(DtagWAInc(3:4),CFDInc(3:4),'ko','markerfacecolor',[255/255 127/255 0/255],'markersize',8); % tag+2
 plot(DtagWAInc(5:6),CFDInc(5:6),'kd','markerfacecolor',[77/255 175/255 74/255],'markersize',8); % tag+4
 
+plot([mean(DtagWAInc(1:2)) mean(DtagWAInc(5:6)) mean(DtagWAInc(7:8))],[GlideTVInc(1) GlideTVInc(5) GlideTVInc(7)],'k--')
 plot(DtagWAInc(1:2),GlideTVInc(1:2),'k^','markerfacecolor',[55/255 126/255 184/255],'markersize',8) % tag
 plot(DtagWAInc(5:6),GlideTVInc(5:6),'kd','markerfacecolor',[77/255 175/255 74/255],'markersize',8) % tag+4
 plot(DtagWAInc(7:8),GlideTVInc(7:8),'ks','markerfacecolor',[228/255 26/255 28/255],'markersize',8) % tag+8
 
+plot([mean(DtagWAInc(1:2)) mean(DtagWAInc(5:6)) mean(DtagWAInc(7:8))],[GlideVFInc(1) GlideVFInc(5) GlideVFInc(7)],'k:')
 plot(DtagWAInc(1:2),GlideVFInc(1:2),'k^','markerfacecolor',[55/255 126/255 184/255],'markersize',8) % tag
 plot(DtagWAInc(5:6),GlideVFInc(5:6),'kd','markerfacecolor',[77/255 175/255 74/255],'markersize',8) % tag+4
 plot(DtagWAInc(7:8),GlideVFInc(7:8),'ks','markerfacecolor',[228/255 26/255 28/255],'markersize',8) % tag+8
+plot(DtagWAInc,GlideVFInc,'k:')
 
 xlabel('Tag Wetted Area Contribution (%)')
 ylabel('Increase in Drag Coefficient (%)')
 adjustfigurefont
 xlim([0 5])
 
-print('Glide_SupplementWaCd','-dsvg','-r300')
+print('Glide_SupplementWaCd','-dpng','-r300')
