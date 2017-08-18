31ed7253ea9bcb5df709bac543444ffb12fcf6db
julievdh <julievdh@mit.edu>
2017-08-18 15:14:15 +0200

    CFD and speed from cameras

--- SlowDown_dolphins2013.m
+++ SlowDown_dolphins2013.m
@@ -43,14 +43,14 @@ tag8 = sum(tag4')+(2*mndiff);
 % plot to the actual curves instead of the interpolated
 velHD = 0:0.1:6;
 % plot drag vs speed for all conditions
-% figure(1); clf; set(gcf,'position',[23   290   850   380])
-% subplot('position',[0.1 0.1 0.4 0.8]); hold on
-% plot(velHD,c_notag(1)*velHD.^c_notag(2),'k','LineWidth',2)
-% plot(velHD,c_tag(1)*velHD.^c_tag(2),'color',[55/255 126/255 184/255],'linewidth',2)
-% plot(velHD,c_tag2(1)*velHD.^c_tag2(2),'color',[255/255 127/255 0/255],'linewidth',2)
-% plot(velHD,c_tag4(1)*velHD.^c_tag4(2),'color',[77/255 175/255 74/255],'linewidth',2)
-% plot(velHD,c_tag6(1)*velHD.^c_tag6(2),':','color',[152/255 78/255 163/255],'linewidth',2)
-% plot(velHD,c_tag8(1)*velHD.^c_tag8(2),':','color',[228/255 26/255 28/255],'linewidth',2)
+figure(1); clf; hold on %set(gcf,'position',[23   290   850   380])
+%subplot('position',[0.1 0.1 0.4 0.8]); hold on
+plot(velHD,c_notag(1)*velHD.^c_notag(2),'k','LineWidth',2)
+plot(velHD,c_tag(1)*velHD.^c_tag(2),'color',[55/255 126/255 184/255],'linewidth',2)
+plot(velHD,c_tag2(1)*velHD.^c_tag2(2),'color',[255/255 127/255 0/255],'linewidth',2)
+plot(velHD,c_tag4(1)*velHD.^c_tag4(2),'color',[77/255 175/255 74/255],'linewidth',2)
+plot(velHD,c_tag6(1)*velHD.^c_tag6(2),'color',[152/255 78/255 163/255],'linewidth',2)
+plot(velHD,c_tag8(1)*velHD.^c_tag8(2),'color',[228/255 26/255 28/255],'linewidth',2)
 
 xlabel('Velocity (m/s)'); ylabel('Drag Force (N)')
 xlim([1 6])
@@ -58,44 +58,46 @@ adjustfigurefont
 
 
 % Find reduced speeds
-Ured_tag = (notag(3)/c_tag(1)).^(1/c_tag(2));
-Ured_tag2 = (notag(3)/c_tag2(1)).^(1/c_tag2(2));
-Ured_tag4 = (notag(3)/c_tag4(1)).^(1/c_tag4(2));
-Ured_tag6 = (notag(3)/c_tag6(1)).^(1/c_tag6(2));
-Ured_tag8 = (notag(3)/c_tag8(1)).^(1/c_tag8(2));
+% INSTEAD OF NOTAG3, DO MEAN CONTROL SPEED = 
+% MN = mean([mean(Kolohe_C),mean(Liko_C),mean(Lono_C),mean(Nainoa_C)]); = almost 4 m/s. 
+Ured_tag = (notag(4)/c_tag(1)).^(1/c_tag(2));
+Ured_tag2 = (notag(4)/c_tag2(1)).^(1/c_tag2(2));
+Ured_tag4 = (notag(4)/c_tag4(1)).^(1/c_tag4(2));
+Ured_tag6 = (notag(4)/c_tag6(1)).^(1/c_tag6(2));
+Ured_tag8 = (notag(4)/c_tag8(1)).^(1/c_tag8(2));
 
 % plot to check
-% plot(Ured_tag8,c_tag8(1)*Ured_tag8.^c_tag8(2),'ks','markerfacecolor',[228/255 26/255 28/255],'markersize',10)
-% plot(Ured_tag6,c_tag6(1)*Ured_tag6.^c_tag6(2),'ko','markerfacecolor',[152/255 78/255 163/255],'markersize',10)
-% plot(Ured_tag4,c_tag4(1)*Ured_tag4.^c_tag4(2),'kd','markerfacecolor',[77/255 175/255 74/255],'markersize',10)
-% plot(Ured_tag2,c_tag2(1)*Ured_tag2.^c_tag2(2),'ko','markerfacecolor',[255/255 127/255 0/255],'markersize',10)
-% plot(Ured_tag,c_tag(1)*Ured_tag.^c_tag(2),'k^','markerfacecolor',[55/255 126/255 184/255],'markersize',10)
-% plot(vel(3),notag(3),'ko','markerfacecolor','k','markersize',10)
-% box on
+plot(Ured_tag8,c_tag8(1)*Ured_tag8.^c_tag8(2),'ks','markerfacecolor',[228/255 26/255 28/255],'markersize',10)
+plot(Ured_tag6,c_tag6(1)*Ured_tag6.^c_tag6(2),'ko','markerfacecolor',[152/255 78/255 163/255],'markersize',10)
+plot(Ured_tag4,c_tag4(1)*Ured_tag4.^c_tag4(2),'kd','markerfacecolor',[77/255 175/255 74/255],'markersize',10)
+plot(Ured_tag2,c_tag2(1)*Ured_tag2.^c_tag2(2),'ko','markerfacecolor',[255/255 127/255 0/255],'markersize',10)
+plot(Ured_tag,c_tag(1)*Ured_tag.^c_tag(2),'k^','markerfacecolor',[55/255 126/255 184/255],'markersize',10)
+plot(vel(4),notag(4),'ko','markerfacecolor','k','markersize',10)
+box on
 
 % calculate percent decrease
-p0t = (abs(Ured_tag-3)/3);
-p0t2 = (abs(Ured_tag2-3)/3);
-p0t4 = (abs(Ured_tag4-3)/3);
-p0t6 = (abs(Ured_tag6-3)/3);
-p0t8 = (abs(Ured_tag8-3)/3);
+p0t = (abs(Ured_tag-4)/4);
+p0t2 = (abs(Ured_tag2-4)/4);
+p0t4 = (abs(Ured_tag4-4)/4);
+p0t6 = (abs(Ured_tag6-4)/4);
+p0t8 = (abs(Ured_tag8-4)/4);
 
 % calculate percent increase if maintain speed
-p0t8_U3 = (tag8(3)-notag(3))/notag(3);
-p0t6_U3 = (tag6(3)-notag(3))/notag(3);
-p0t4_U3 = (sum(tag4(3,:))-notag(3))/notag(3);
-p0t2_U3 = (sum(tag2(3,:))-notag(3))/notag(3);
-p0t_U3 = (sum(tag(3,:))-notag(3))/notag(3);
+p0t8_U4 = (tag8(4)-notag(4))/notag(4);
+p0t6_U4 = (tag6(4)-notag(4))/notag(4);
+p0t4_U4 = (sum(tag4(4,:))-notag(4))/notag(4);
+p0t2_U4 = (sum(tag2(4,:))-notag(4))/notag(4);
+p0t_U4 = (sum(tag(4,:))-notag(4))/notag(4);
 
 %% plot alternative - that they can maintain speed but increase drag
-% plot(3,tag8(3),'s','markeredgecolor',[228/255 26/255 28/255],'markersize',10,'linewidth',2)
-% plot(3,tag6(3),'o','markeredgecolor',[152/255 78/255 163/255],'markersize',10,'linewidth',2)
-% plot(3,sum(tag4(3,:)),'d','markeredgecolor',[77/255 175/255 74/255],'markersize',10,'linewidth',2)
-% plot(3,sum(tag2(3,:)),'o','markeredgecolor',[255/255 127/255 0/255],'markersize',10,'linewidth',2)
-% plot(3,sum(tag(3,:)),'^','markeredgecolor',[55/255 126/255 184/255],'markersize',10,'linewidth',2)
+plot(4,tag8(4),'s','markeredgecolor',[228/255 26/255 28/255],'markersize',10,'linewidth',2)
+plot(4,tag6(4),'o','markeredgecolor',[152/255 78/255 163/255],'markersize',10,'linewidth',2)
+plot(4,sum(tag4(4,:)),'d','markeredgecolor',[77/255 175/255 74/255],'markersize',10,'linewidth',2)
+plot(4,sum(tag2(4,:)),'o','markeredgecolor',[255/255 127/255 0/255],'markersize',10,'linewidth',2)
+plot(4,sum(tag(4,:)),'^','markeredgecolor',[55/255 126/255 184/255],'markersize',10,'linewidth',2)
 % 
-% cd /Users/julievanderhoop/Documents/MATLAB/DQ/DQ2013/AnalysisFigs
-% print('CFDresults','-dsvg','-r300')
+cd /Users/julievanderhoop/Documents/MATLAB/DQ/DQ2013/AnalysisFigs
+print('CFDresults','-dsvg','-r300')
 
 %% for Introduction
 % figure(2); hold on
@@ -156,8 +158,8 @@ plot(velHD,c_notag(1)*velHD.^c_notag(2),'k','LineWidth',2); box on
 plot(velHD,c_tag(1)*velHD.^c_tag(2),'color',[55/255 126/255 184/255],'linewidth',2)
 plot(velHD,c_tag2(1)*velHD.^c_tag2(2),'color',[255/255 127/255 0/255],'linewidth',2)
 plot(velHD,c_tag4(1)*velHD.^c_tag4(2),'color',[77/255 175/255 74/255],'linewidth',2)
-plot(velHD,c_tag6(1)*velHD.^c_tag6(2),':','color',[152/255 78/255 163/255],'linewidth',2)
-plot(velHD,c_tag8(1)*velHD.^c_tag8(2),':','color',[228/255 26/255 28/255],'linewidth',2)
+plot(velHD,c_tag6(1)*velHD.^c_tag6(2),'color',[152/255 78/255 163/255],'linewidth',2)
+plot(velHD,c_tag8(1)*velHD.^c_tag8(2),'color',[228/255 26/255 28/255],'linewidth',2)
 xlim([1 6])
 
 
@@ -196,7 +198,26 @@ plot(Lpath(:,1),Lpath(:,2),'k-')
 text(1.15,925,'B','fontsize',18,'fontweight','bold')
 xlabel('Velocity (m/s)'); ylabel('Drag Force (N)')
 
-%%
+%% Store Average change in velocity
+slowKU(1) = (nanmean(T.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowKU(2) = (nanmean(T2.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowKU(3) = (nanmean(T4.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowKU(4) = (nanmean(T6.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowKU(5) = (nanmean(T8.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+
+slowKL(1) = (nanmean(T.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowKL(2) = (nanmean(T2.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowKL(3) = (nanmean(T4.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowKL(4) = (nanmean(T6.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowKL(5) = (nanmean(T8.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+
+Kmean(1) = mean([mean(C.Lspeed) mean(C.Uspeed)]);
+Kmean(2) = mean([nanmean(T.Lspeed) nanmean(T.Uspeed)]);
+Kmean(3) = mean([nanmean(T2.Lspeed) nanmean(T2.Uspeed)]);
+Kmean(4) = mean([mean(T4.Lspeed) mean(T4.Uspeed)]);
+Kmean(5) = mean([nanmean(T6.Lspeed) nanmean(T6.Uspeed)]);
+Kmean(6) = mean([nanmean(T8.Lspeed) nanmean(T8.Uspeed)]);
+
 %% add velocities from loading/unloading trials
 % plot on figure % Lono first, then Kolohe 
 figure(2); subplot('position',[0.1 0.1 0.4 0.8]); hold on
@@ -204,8 +225,8 @@ plot(velHD,c_notag(1)*velHD.^c_notag(2),'k','LineWidth',2); box on
 plot(velHD,c_tag(1)*velHD.^c_tag(2),'color',[55/255 126/255 184/255],'linewidth',2)
 plot(velHD,c_tag2(1)*velHD.^c_tag2(2),'color',[255/255 127/255 0/255],'linewidth',2)
 plot(velHD,c_tag4(1)*velHD.^c_tag4(2),'color',[77/255 175/255 74/255],'linewidth',2)
-plot(velHD,c_tag6(1)*velHD.^c_tag6(2),':','color',[152/255 78/255 163/255],'linewidth',2)
-plot(velHD,c_tag8(1)*velHD.^c_tag8(2),':','color',[228/255 26/255 28/255],'linewidth',2)
+plot(velHD,c_tag6(1)*velHD.^c_tag6(2),'color',[152/255 78/255 163/255],'linewidth',2)
+plot(velHD,c_tag8(1)*velHD.^c_tag8(2),'color',[228/255 26/255 28/255],'linewidth',2)
 xlim([1 6])
 
 
@@ -233,6 +254,35 @@ plot(nanmean(T4.Lspeed),c_tag4(1)*nanmean(T4.Lspeed).^c_tag4(2),'ko','markerface
 plot(nanmean(T6.Lspeed),c_tag6(1)*nanmean(T6.Lspeed).^c_tag6(2),'ko','markerfacecolor',[152/255 78/255 163/255],'markersize',10)
 plot(nanmean(T8.Lspeed),c_tag8(1)*nanmean(T8.Lspeed).^c_tag8(2),'ko','markerfacecolor',[228/255 26/255 28/255],'markersize',10)
 
+%% calculate change in speed
+
+slowLU(1) = (nanmean(T.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowLU(2) = (nanmean(T2.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowLU(3) = (nanmean(T4.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowLU(4) = (nanmean(T6.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+slowLU(5) = (nanmean(T8.Uspeed)-mean(C.Uspeed))/mean(C.Uspeed);
+
+slowLL(1) = (nanmean(T.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowLL(2) = (nanmean(T2.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowLL(3) = (nanmean(T4.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowLL(4) = (nanmean(T6.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+slowLL(5) = (nanmean(T8.Lspeed)-mean(C.Lspeed))/mean(C.Lspeed);
+
+Lmean(1) = mean([mean(C.Lspeed) mean(C.Uspeed)]);
+Lmean(2) = mean([nanmean(T.Lspeed) nanmean(T.Uspeed)]);
+Lmean(3) = mean([mean(T2.Lspeed) mean(T2.Uspeed)]);
+Lmean(4) = mean([nanmean(T4.Lspeed) nanmean(T4.Uspeed)]);
+Lmean(5) = mean([nanmean(T6.Lspeed) nanmean(T6.Uspeed)]);
+Lmean(6) = mean([nanmean(T8.Lspeed) nanmean(T8.Uspeed)]);
+
+% mean speed 
+mean([Kmean; Lmean])
+std([Kmean; Lmean])
+
+% mean slow down with loading
+mean([slowKL; slowKU; slowLU; slowLL])
+std([slowKL; slowKU; slowLU; slowLL])
+
 %% lines in between
 % c - t - t2 - t4 - t6 - t8 % i guess the direction of plotting doesn't
 % actually matter
@@ -246,7 +296,7 @@ text(1.15,925,'A','fontsize',18,'fontweight','bold')
 xlabel('Velocity (m/s)'); ylabel('Drag Force (N)')
 adjustfigurefont
 
-
-print -dpng SpeedPersp_UnloadLoadBoth
+set(gcf,'paperpositionmode','auto')
+print -dsvg SpeedPersp_UnloadLoadBoth
 
 
