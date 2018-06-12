a5099d5e62ab6b9d33f527e3a415b60dee7990f5
julievdh <julievdh@mit.edu>
2018-06-12 09:20:06 +0200

    Get breath counts data from DQ or Sarasota for Niels

---
+++ BreathDataforNiels.m
@@ -0,0 +1,9 @@
+% breath data for Niels
+clear
+R = loadaudit('tt13_266a');
+
+breath = findbreathcues(R);
+ibi = diff(breath(:,1)); ibi = ibi(ibi>0.5);
+dur = (max(breath(:,1))-min(breath(:,1)))/60;
+
+tbl = [dur min(ibi) max(ibi) median(ibi) mean(ibi) std(ibi)];
